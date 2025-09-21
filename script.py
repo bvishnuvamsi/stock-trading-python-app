5e3f6f77fbb2e68c2552c2b3c84a4b7f3ccad4f9
bvishnuvamsi <bvamsisrinivas1@gmail.com>
2025-09-21 17:24:22 -0500

    Added Scheduler and learn about Crontab

--- script.py
+++ script.py
@@ -1,65 +1,65 @@
 import requests
-import os
 import csv
+import os
 from dotenv import load_dotenv
 load_dotenv()
 import time
 
-
 POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
-#print(POLYGON_API_KEY)
 
 LIMIT = 1000
 
-URL = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
+def run_stock_job():
+    URL = f'https://api.polygon.io/v3/reference/tickers?market=stocks&active=true&order=asc&limit={LIMIT}&sort=ticker&apiKey={POLYGON_API_KEY}'
+    response = requests.get(URL)
+    #print(response.json())
 
+    tickers = []
 
-response = requests.get(URL)
-#print(response.json())
-
-tickers = []
-
-data = response.json()
-#print(data.keys())
-
-#print(data['next_url'])
-
-for ticker in data['results']:
-    tickers.append(ticker)
-print(len(tickers))
-
-while 'next_url' in data:
-    print('requesting next page', data['next_url'])
-    time.sleep(15)
-    response = requests.get(data['next_url'] + f'&apiKey={POLYGON_API_KEY}')
     data = response.json()
-    #print(data)
-    print(len(tickers))
+    #print(data.keys())
+
+    #print(data['next_url'])
+
     for ticker in data['results']:
         tickers.append(ticker)
+    print(len(tickers))
 
-print(len(tickers))
+    while 'next_url' in data:
+        print('requesting next page', data['next_url'])
+        time.sleep(15)
+        response = requests.get(data['next_url'] + f'&apiKey={POLYGON_API_KEY}')
+        data = response.json()
+        #print(data)
+        print(len(tickers))
+        for ticker in data['results']:
+            tickers.append(ticker)
 
-fields = [
-    'ticker',
-    'name',
-    'market',
-    'locale',
-    'primary_exchange',
-    'type',
-    'active',
-    'currency_name',
-    'cik',
-    'composite_figi',
-    'share_class_figi',
-]
+    print(len(tickers))
 
-ouput_csv = 'tickers.csv'
+    fields = [
+        'ticker',
+        'name',
+        'market',
+        'locale',
+        'primary_exchange',
+        'type',
+        'active',
+        'currency_name',
+        'cik',
+        'composite_figi',
+        'share_class_figi',
+    ]
 
-with open(ouput_csv , mode = 'w', newline='', encoding='utf-8') as f:
-    writer = csv.DictWriter(f, fieldnames=fields, extrasaction='ignore', restval='')
-    writer.writeheader()
-    for row in tickers:
-        writer.writerow(row)
+    ouput_csv = 'tickers.csv'
 
-print(f"Wrote {len(tickers)} rows to {ouput_csv}")
+    with open(ouput_csv , mode = 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fields, extrasaction='ignore', restval='')
+        writer.writeheader()
+        for row in tickers:
+            writer.writerow(row)
+
+    print(f"Wrote {len(tickers)} rows to {ouput_csv}")
+
+if __name__ == '__main__':
+    run_stock_job()
